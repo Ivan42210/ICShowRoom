aed3fd5f21fe224c7b37b3c46e173df28a7945b4
Ivan42210 <ivan.cha.42@outlook.fr>
2023-06-27 23:00:49 +0200

    test responsive

--- src/TestPage/index.jsx
+++ src/TestPage/index.jsx
@@ -1,9 +1,35 @@
-import CardSection from '../Components/CardSection'
+import CardSection from '../Components/cardSection'
 import './overrideTest.css'
 import { soberDatas } from '../Templates/sober/datas/dataSober'
 import Card from '../Components/Card'
+import { useEffect, useState } from 'react'
 
 export default function Test() {
+
+    const [width, setWindowWith ] = useState(0)
+
+    const updateDimension = () => {
+        const width = window.innerWidth
+        setWindowWith(width)
+        
+    }
+
+    useEffect( () => {
+        updateDimension();
+        window.addEventListener("resize", updateDimension);
+       
+        return ()=> window.removeEventListener("resize", updateDimension);
+        
+    }, [])
+
+   const responsive = {
+        smartphoneView : width < 780,
+        tabletView : width < 1080 && width > 781,
+        desktop : width > 1280,
+    }
+
+    console.log(responsive.desktop)
+  
 
 
 
@@ -11,7 +37,7 @@ export default function Test() {
         <>
             <h2>This is a test</h2>
 
-            <CardSection colNumber={2}>
+            <CardSection colNumber={4}>
                 {soberDatas.map(({ id, title, thumbnail, description, author, authorPic, date, index }) => (
                     <Card
                         link='sober'
